43dc54f31d1e615420864fb439ee514aa8da605a
Hameed Abdul <hameedabdulrashid@gmail.com>
2016-10-25 16:17:16 -0500

    Default Commit
    This is the restart of the initally project... not much but just incase
    my computer dies again

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 speed;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.velocity = new Vector3( speed.x ,rb.velocity.y, rb.velocity.z);
+	}
+}
